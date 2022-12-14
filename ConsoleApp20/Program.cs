3cc3368a03bcf61a714dfb574117d8b37858c5c7
Артём Коваленко <ak7527203@gmail.com>
2022-12-14 20:22:27 +0200

    Add project files.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string[] text = Array.Empty<string>();
+            text = Console.ReadLine().Split();
+            Solution solution = new(text);
+            Console.WriteLine(solution.getSolution(text));
+        }
+    }
+}
---
+++ ConsoleApp20/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Solution
+    {
+        private string[] text = Array.Empty<string>();
+        private static string temp = "";
+        public Solution(string[] text)
+        {
+            Text = text;
+        }
+
+        public string[] Text { get; set; }
+
+        public string[] getSolution(string[] text)
+        {
+            temp = text[0];
+            text[0] = text[text.Length - 1];
+            text[text.Length - 1] = temp;
+            return text;
+        }
+    }
+}
